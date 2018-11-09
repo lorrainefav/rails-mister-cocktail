fd81f6d4e359a09b4e2d5c45db9e1c72b95284b3
Lorraine Favreau <lorraine.favreau@gmail.com>
2018-11-09 15:56:57 +0100

    done

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ gem 'bootstrap-sass', '~> 3.3.7'
 
 gem 'font-awesome-sass', '~> 5.0.9'
 
+gem 'cloudinary'
+
 gem 'simple_form'
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
@@ -48,6 +50,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'dotenv-rails'
 end
 
 group :development do
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-  get 'doses/new'
-  get 'doses/create'
-  get 'doses/destroy'
-  get 'cocktails/index'
-  get 'cocktails/show'
-  get 'cocktails/new'
-  get 'cocktails/create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [ :index, :show, :new, :create ] do
+
+
+  root to: "cocktails#index"
+  resources :cocktails, only: [:show, :new, :create ] do
     resources :doses, only: [ :new, :create]
   end
   resources :doses, only: [:destroy]
