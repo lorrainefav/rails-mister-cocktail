24f3b6777bd8a37bd6db586ce9c61605ac189db1
Lorraine Favreau <lorraine.favreau@gmail.com>
2018-11-09 12:40:26 +0100

    done

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -16,11 +16,9 @@ class DosesController < ApplicationController
   end
 
   def destroy
-    @cocktail = Cocktail.find(params[:cocktail_id])
-    @dose = Dose.new(dose_params)
-    @dose.cocktail = @cocktail
+    @dose = Dose.find(params[:id])
     @dose.destroy
-    redirect_to cocktail_path(@cocktail)
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
